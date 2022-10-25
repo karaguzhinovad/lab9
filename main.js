ceb5b42b019ab2ea2d9f4f4a3ee4b3b9afddd74a
Dariya Karaguzhinova <dariyakaraguzhinova@MacBook-Air-Dariya.local>
2022-10-25 19:46:33 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function evenodd(n) {
+r = (n % 2)
+	if (r == 0) {console.log(n, 'is even')}
+else {console.log(n, 'is odd')}
+}
+evenodd(7)
+
+function twonumbers(a,b) {
+	if (a < b) {console.log(b, 'is the greatest number between these two numbers');
+	}
+	else {console.log(a, 'is the greatest number between these two numbers');
+	}
+}
+twonumbers(13, 15);
+
+function perimeter(l,w)
+{
+	p = (l + w) * 2;
+
+console.log(p, "is a perimeter of the rectangle given by two sides")}
+
+perimeter(2,3)
+
+
